97c850c95cafbf1006d53d31aa4009d00a94b3be
diogenes1027 <a01653251@itesm.mx>
2022-06-17 22:20:01 -0500

    Springboot generated
    Springboot generated

---
+++ awshack/src/main/java/com/adawomanhack/awshack/AwshackApplication.java
@@ -0,0 +1,13 @@
+package com.adawomanhack.awshack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwshackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwshackApplication.class, args);
+	}
+
+}
---
+++ awshack/src/test/java/com/adawomanhack/awshack/AwshackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adawomanhack.awshack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwshackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
